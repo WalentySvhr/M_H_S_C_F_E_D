8d9f513d8d2abc6108bda54a2eb6470932e33cfc
WalentySvhr <limoni.ua@gmail.com>
2023-04-28 21:41:17 +0300

    first-elemen-four-box-main-page

--- script.js
+++ script.js
@@ -47,17 +47,83 @@ function myFunctionToggle() {
     dropboxTwo.classList.toggle("program-box-line");
 }
 
+var buttonTwo = document.querySelector(".program-button-two");
 
+buttonTwo.addEventListener('click', function() {
+  if (buttonTwo.textContent === 'Закрити') {
+    buttonTwo.textContent = 'Відкрити';
+  } else {
+    buttonTwo.textContent = 'Закрити';
+  }
+});
 
 
 
 
 var dropboxThree = document.querySelector(".program-box-three");
+var buttonThree = document.querySelector('.program-button-three');
 
 function myFunctionThreeToggle() {
-
     dropboxThree.classList.toggle("program-box-line");
-    
   }
-  
+ 
+  buttonThree.addEventListener('click', function() {
+  if (buttonThree.textContent === 'Закрити') {
+    buttonThree.textContent = 'Відкрити';
+  } else {
+    buttonThree.textContent = 'Закрити';
+  }
+});
+
+
+var dropboxFour = document.querySelector(".program-box-four");
+
+function myFunctionFourToggle() {
+  dropboxFour.classList.toggle("program-box-line");
+}
+
+var buttonFour = document.querySelector(".program-button-four");
+
+buttonFour.addEventListener('click', function() {
+  if (buttonFour.textContent === 'Закрити') {
+    buttonFour.textContent = 'Відкрити';
+  } else {
+    buttonFour.textContent = 'Закрити';
+  }
+});
+
+
+var dropboxFive = document.querySelector(".program-box-five");
+
+function myFunctionFiveToggle() {
+  dropboxFive.classList.toggle("program-box-line");
+}
+
+var buttonFive = document.querySelector(".program-button-five");
+
+buttonFive.addEventListener('click', function() {
+  if (buttonFive.textContent === 'Закрити') {
+    buttonFive.textContent = 'Відкрити';
+  } else {
+    buttonFive.textContent = 'Закрити';
+  }
+});
+
+
+var dropboxSixe = document.querySelector(".program-box-sixe");
+
+function myFunctionSixeToggle() {
+  dropboxSixe.classList.toggle("program-box-line");
+}
+
+var buttonSixe = document.querySelector(".program-button-sixe");
+
+buttonSixe.addEventListener('click', function() {
+  if (buttonSixe.textContent === 'Закрити') {
+    buttonSixe.textContent = 'Відкрити';
+  } else {
+    buttonSixe.textContent = 'Закрити';
+  }
+});
+
 /****************************************** */
